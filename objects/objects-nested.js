cb13ebf970d2d9c49d0b5333e9b1661d0f8c3341
Amber Mustafa <tptb123666@gmail.com>
2022-11-19 20:53:36 +0530

    using an array inside the object

--- objects/objects-nested.js
+++ objects/objects-nested.js
@@ -13,3 +13,26 @@ let myCar2 = "Nancy has a " + cars.brand + " " + cars.model + " which is " + car
 
 console.log(myCar1);
 console.log(myCar2);
+
+// Adding an array inside the object
+// cars is an array of objects
+const myObj = {
+    name: "John",
+    age: 30,
+    cars: [
+        { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+        { name: "BMW", models: ["320", "X3", "X5"] },
+        { name: "Fiat", models: ["500", "Panda"] }
+    ]
+}
+
+// Iteration
+var x;
+for (let i in myObj.cars) {
+    x += myObj.cars[i].name;
+    for (let j in myObj.cars[i].models) {
+        x += myObj.cars[i].models[j];
+    }
+
+    console.log(x);
+}
